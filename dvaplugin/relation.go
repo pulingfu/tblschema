ce8f787a06dae02307771b8dd8c2b54058910285
kongyu <pulingfu@juzikong.com>
2025-01-07 15:44:39 +0800

    docs: ✏️ 补充注释和新增选项

--- dvaplugin/relation.go
+++ dvaplugin/relation.go
@@ -9,16 +9,105 @@ import (
 	"gorm.io/gorm"
 )
 
+// 关系类型
 type RELATION_TYPE = int
 
 const (
-	HAS_ONE   RELATION_TYPE = iota //加载一个子对象
-	HAS_MANY                       //加载多个子对象/数组
-	BELONG_TO                      //直接加载数据给到父节点
+	//一对一或多对一，父元素是1个或者多个，每个父元素里有一个子元素
+	// 比如：
+	/*
+		{
+			"p_name": "父亲",
+			"p_age": "年龄",
+			"one_child": {
+				"s_name": "子名",
+				"c_age": "年龄"
+			}
+		}
+	*/
+	// 或者
+	/*
+		[
+			{
+				"p_name": "父亲1",
+				"p_age": "年龄",
+				"one_child": {
+					"s_name": "子名1",
+					"c_age": "年龄"
+				}
+			},
+			{
+				"p_name": "父亲2",
+				"p_age": "年龄",
+				"one_child": {
+					"s_name": "子名2",
+					"c_age": "年龄"
+				}
+			}
+		]
+	*/
+	HAS_ONE RELATION_TYPE = iota
+
+	//加载多个子对象/数组  一对多或多对多，父元素是1个或者多个，每个父元素里有多个子元素
+	// 比如 :
+	/*
+		{
+		    "p_name": "父亲",
+		    "p_age": "年龄",
+		    "children": [
+		        {
+		            "s_name": "孩子1",
+		            "c_age": "年龄"
+		        },
+		        {
+		            "s_name": "孩子2",
+		            "c_age": "年龄"
+		        }
+		    ]
+		}
+	*/
+	// 或者
+	/*
+		[
+		    {
+		        "p_name": "父亲1",
+		        "p_age": "年龄",
+		        "children": [
+		            {
+		                "s_name": "孩子1",
+		                "c_age": "年龄"
+		            },
+		            {
+		                "s_name": "孩子2",
+		                "c_age": "年龄"
+		            }
+		        ]
+		    },
+		    {
+		        "p_name": "父亲2",
+		        "p_age": "年龄",
+		        "children": [
+		            {
+		                "s_name": "儿子1",
+		                "c_age": "年龄"
+		            },
+		            {
+		                "s_name": "女儿1",
+		                "c_age": "年龄"
+		            }
+		        ]
+		    }
+		]
+	*/
+	HAS_MANY
+
+	//直接加载数据给到父节点 一对一
+	BELONG_TO
 )
 
 type RELARTION_NET map[string]*RelationLoader
 
+// 关系网络
 type RelationLoader struct {
 	input         interface{}   //输入
 	result        interface{}   //输出
@@ -35,16 +124,22 @@ type RelationLoader struct {
 	OpenPrintErrStack bool          //是否开启打印错误堆栈
 }
 
+// GetInput 获取输入
 func (r *RelationLoader) GetInput() interface{} {
 	return r.input
 }
 
+// GetResult 获取输出
 func (r *RelationLoader) GetResult() interface{} {
 	return r.result
 }
+
+// Error 获取错误
 func (r *RelationLoader) Error() error {
 	return r.err
 }
+
+// NewRelationLoader 初始化关系网络
 func NewRelationLoader(input interface{}, OpenPrintErrStack bool) *RelationLoader {
 	return &RelationLoader{
 		input:             input,
@@ -53,6 +148,38 @@ func NewRelationLoader(input interface{}, OpenPrintErrStack bool) *RelationLoade
 	}
 }
 
+// AddRelationWithOptions 在关系网络上添加一个关系
+func (r *RelationLoader) AddRelationWithOptions(opt *RelationOptions) *RelationLoader {
+	var err error
+	func(err *error) {
+		defer catch(err, r.OpenPrintErrStack)
+		rls := strings.Split(opt.Relation, ".")
+		new_sr := &RelationLoader{
+			childModel:    opt.Child,
+			fakey:         opt.Fakey,
+			sukey:         opt.Sukey,
+			relation_type: opt.RelationType,
+		}
+
+		if opt.CompareFunc != nil {
+			new_sr.compareFunc = opt.CompareFunc
+		}
+		if opt.Cdb != nil {
+			new_sr.cdb = opt.Cdb
+		}
+		if opt.SubModifyFunc != nil {
+			new_sr.subModifyFunc = opt.SubModifyFunc
+		}
+
+		r.setRelation(rls, new_sr)
+	}(&err)
+	if err != nil {
+		r.err = err
+	}
+	return r
+}
+
+// AddRelation 在关系网络上添加一个关系
 func (r *RelationLoader) AddRelation(relation_type RELATION_TYPE, relation, fakey, sukey string,
 	Child interface{}, compareFunc CompareFun, cdb *gorm.DB, anonps ...interface{}) *RelationLoader {
 	var err error
@@ -97,6 +224,7 @@ func (r *RelationLoader) setRelation(relations []string, sr *RelationLoader) *Re
 	return r
 }
 
+// LoadResult 加载关系数据
 func (r *RelationLoader) LoadResult(db *gorm.DB) *RelationLoader {
 	var err error
 	func(err *error) {
@@ -195,4 +323,71 @@ func (r *RelationLoader) load(db *gorm.DB) {
 		}
 	}
 
+}
+
+// RelationOptions 关系选项
+type RelationOptions struct {
+	RelationType  RELATION_TYPE //必填 父子关系类型
+	Relation      string        //必填 父子关系名
+	Fakey         string        //必填 父元素对应的key
+	Sukey         string        //必填 子元素对应的key
+	Child         interface{}   //必填 子元素模型
+	CompareFunc   CompareFun    //可选 自定义父子映射函数
+	Cdb           *gorm.DB      //可选 自定义查询子数据的条件db
+	SubModifyFunc SubModifyFunc //可选 自定义 父子数据修改函数
+}
+
+func NewRelationOptions() *RelationOptions {
+	return &RelationOptions{}
+}
+
+// SetRelationType 设置关系类型
+func (r *RelationOptions) SetRelationType(relation_type RELATION_TYPE) *RelationOptions {
+	r.RelationType = relation_type
+	return r
+}
+
+// SetRelation 设置关系名
+func (r *RelationOptions) SetRelation(relation string) *RelationOptions {
+	r.Relation = relation
+	return r
+}
+
+// SetFakey 设置父级数据对应的key
+func (r *RelationOptions) SetFakey(fakey string) *RelationOptions {
+	r.Fakey = fakey
+	return r
+}
+
+// SetSukey 设置子级数据对应的key
+func (r *RelationOptions) SetSukey(sukey string) *RelationOptions {
+	r.Sukey = sukey
+	return r
+}
+
+// SetChild 设置子数据的model 一般为gorm 对应的struct
+func (r *RelationOptions) SetChild(child interface{}) *RelationOptions {
+	r.Child = child
+	return r
+}
+
+// SetCompareFunc 设置自定义比较函数 用于连接父数据和子数据的关键判断
+func (r *RelationOptions) SetCompareFunc(compareFunc CompareFun) *RelationOptions {
+	r.CompareFunc = compareFunc
+	return r
+}
+
+// SetCdb 设置自定义查询子数据的条件db
+func (r *RelationOptions) SetCdb(cdb *gorm.DB) *RelationOptions {
+	r.Cdb = cdb
+	return r
+}
+
+// SetSubModifyFunc 设置自定义父子数据修改
+// 该方法在 最终数据连接的时候执行，用于对父数据或者子数据自定义增加减少字段或者修改字段等，内容可高度自定义
+// subModifyFunc(p, s gjson.Result) (gjson.Result, gjson.Result)
+// p是父数据 s是子数据 返回 p,s 父子数据，即内容可以自定义修改，但是不要丢失对应的映射key
+func (r *RelationOptions) SetSubModifyFunc(subModifyFunc SubModifyFunc) *RelationOptions {
+	r.SubModifyFunc = subModifyFunc
+	return r
 }
