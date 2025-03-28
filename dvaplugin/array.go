dbdacd43f6b10e2744c9cf8df63145f141d04188
kongyu <pulingfu@juzikong.com>
2025-03-28 14:02:05 +0800

    test: 💍 commint

--- dvaplugin/array.go
+++ dvaplugin/array.go
@@ -1,17 +1,18 @@
 package dvaplugin
 
+// Slicer  切片处理
 type Slicer[T any] struct {
 	Input []T
 }
 
-// 从切片创建切片对象
+// FromSlice 从切片创建切片对象
 func FromSlice[T any](input []T) *Slicer[T] {
 	return &Slicer[T]{
 		Input: input,
 	}
 }
 
-// 判断元素是否在数组中的方法
+// InSilce  判断元素是否在数组中的方法
 func (s *Slicer[T]) InSilce(item T, equal func(a, b T) bool) bool {
 	for _, v := range s.Input {
 		if equal(v, item) {
@@ -21,7 +22,7 @@ func (s *Slicer[T]) InSilce(item T, equal func(a, b T) bool) bool {
 	return false
 }
 
-// 判断是否有元素符合条件
+// Contains 判断是否有元素符合条件
 func (s *Slicer[T]) Contains(equal func(b T) bool) bool {
 	for _, v := range s.Input {
 		if equal(v) {
@@ -31,7 +32,7 @@ func (s *Slicer[T]) Contains(equal func(b T) bool) bool {
 	return false
 }
 
-// 分割数组的方法
+// Divide 分割数组
 func (s *Slicer[T]) Divide(_f_div func(T) bool) (hits []T, misses []T) {
 	hits = make([]T, 0, len(s.Input))
 	misses = make([]T, 0, len(s.Input))
@@ -45,7 +46,7 @@ func (s *Slicer[T]) Divide(_f_div func(T) bool) (hits []T, misses []T) {
 	return hits, misses
 }
 
-// 查找数组中符合条件的元素
+// Take 根据条件返回第一个符合条件的元素
 func (s *Slicer[T]) Take(_f func(T) bool) T {
 	for _, v := range s.Input {
 		if _f(v) {
@@ -55,6 +56,7 @@ func (s *Slicer[T]) Take(_f func(T) bool) T {
 	return *new(T)
 }
 
+// Find 查找数组中符合条件的元素
 func (s *Slicer[T]) Find(_f func(T) bool) []T {
 	var matches = make([]T, 0, len(s.Input))
 	for _, v := range s.Input {
